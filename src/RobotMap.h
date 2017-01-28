b2dedd347f88a47b00d35c3575c4153cddce3ce1
Liam Kinne <kinneliam@gmail.com>
2017-01-28 22:00:21 +1100

    Map update

--- src/RobotMap.h
+++ src/RobotMap.h
@@ -1,16 +1,27 @@
 #ifndef ROBOTMAP_H
 #define ROBOTMAP_H
 
+// Drive Train
 constexpr int DRIVETRAIN_LEFT_MOTOR = 0;
 constexpr int DRIVETRAIN_RIGHT_MOTOR = 1;
+
+// Lifting
+const int LIFTING_SPARK_LEFT_PIN = 3;
+const int LIFTING_SPARK_RIGHT_PIN = 4;
+
+// Shooter
+const int SHOOTER_TALON_PIN = 2;
+const bool SHOOTER_DEFAULT_DIRECTION = 1; // Inverted
+const double SHOOTER_DEFAULT_SPEED = 0.95;
+
+// Ball Pickup
+const int SUCKER_TALON_PIN = 5;
+const bool SUCKER_DEFAULT_DIRECTION = 1; // Inverted
 
 // Driver Mapping
 constexpr int DRIVER_JOY_NUM = 0;
 
-
 // Assistant Mapping
 constexpr int ASSISTANT_JOY_NUM = 1;
-
-const int SHOOTER_TALON_PIN = 2;
 
 #endif  // ROBOTMAP_H
