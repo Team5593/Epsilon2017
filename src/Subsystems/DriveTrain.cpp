90198d85815dfa857b57507b7d1eda2726ce3f39
Liam Kinne <kinneliam@gmail.com>
2017-02-11 22:37:03 +1100

    Ultrasonic Sensor

--- src/Subsystems/DriveTrain.cpp
+++ src/Subsystems/DriveTrain.cpp
@@ -1,18 +1,22 @@
 #include "DriveTrain.h"
 #include "../Commands/Drive.h"
 
-DriveTrain::DriveTrain(int leftMotorChannel, int rightMotorChannel, int encLeftChannelA, int encLeftChannelB, int encRightChannelA, int encRightChannelB, int analogGyroChannel) :
+DriveTrain::DriveTrain(int leftMotorChannel, int rightMotorChannel, int encLeftChannelA, int encLeftChannelB, int encRightChannelA, int encRightChannelB, int analogGyroChannel, int ultraLeftChannel, int ultraRightChannel) :
 	frc::Subsystem("DriveTrain"),
- _robotDrive(leftMotorChannel, rightMotorChannel),
- _encLeft(encLeftChannelA, encLeftChannelB),
- _encRight(encRightChannelA, encRightChannelB),
- _gyro(analogGyroChannel)
+	_robotDrive(leftMotorChannel, rightMotorChannel),
+	_encLeft(encLeftChannelA, encLeftChannelB),
+	_encRight(encRightChannelA, encRightChannelB),
+	_gyro(analogGyroChannel),
+	_ultraFrontLeft(ultraLeftChannel),
+	_ultraFrontRight(ultraRightChannel)
 {
 	_encLeft.SetDistancePerPulse(_distancePerPulse);
 	_encLeft.SetReverseDirection(true);
 	_encRight.SetDistancePerPulse(_distancePerPulse);
 	_robotDrive.SetExpiration(0.1);
 	_robotDrive.SetSafetyEnabled(false);
+	_ultraFrontLeft.SetDistanceUnits(_ultraFrontLeft.kInches);
+	_ultraFrontRight.SetDistanceUnits(_ultraFrontRight.kInches);
 }
 
 void DriveTrain::InitDefaultCommand() {
@@ -23,6 +27,10 @@ void DriveTrain::InitDefaultCommand() {
 void DriveTrain::TankDrive(double left, double right) {
 	_robotDrive.TankDrive(left, right);
 
+}
+
+void DriveTrain::Stop() {
+	_robotDrive.TankDrive(0.0, 0.0);
 }
 
 void DriveTrain::GyroCalibrate() {
@@ -58,6 +66,10 @@ void DriveTrain::EncResetAll(){
 	EncResetRight();
 }
 
-void DriveTrain::Stop() {
-	_robotDrive.TankDrive(0.0, 0.0);
+double DriveTrain::GetUltraFrontLeft() {
+	return _ultraFrontLeft.GetRangeInches();
+}
+
+double DriveTrain::GetUltraFrontRight() {
+	return _ultraFrontRight.GetRangeInches();
 }
--- src/Subsystems/DriveTrain.h
+++ src/Subsystems/DriveTrain.h
@@ -5,6 +5,7 @@
 #include <RobotDrive.h>
 #include <Encoder.h>
 #include <AnalogGyro.h>
+#include <Ultrasonic.h>
 #include "RobotMap.h"
 
 class DriveTrain : public frc::Subsystem {
@@ -17,26 +18,30 @@ private:
 	frc::Encoder _encLeft;
 	frc::Encoder _encRight;
 	frc::AnalogGyro _gyro;
+	frc::Ultrasonic _ultraFrontLeft;
+	frc::Ultrasonic _ultraFrontRight;
 
 	double _distancePerPulse = 4 * 3.14159 / 250;
 
 public:
-	DriveTrain(int leftMotorChannel, int rightMotorChannel, int encLeftChannelA, int encLeftChannelB, int encRightChannelA, int encRightChannelB, int analogGyroChannel);
+	DriveTrain(int leftMotorChannel, int rightMotorChannel, int encLeftChannelA, int encLeftChannelB, int encRightChannelA, int encRightChannelB, int analogGyroChannel, int ultraLeftChannel, int ultraRightChannel);
 	void InitDefaultCommand();
 	void TankDrive(double left, double right);
+	void Stop();
 
 	// Gyro
 	void GyroCalibrate();
 	void GyroReset();
 	double GetGyroAngle();
-	//Encoders
+	// Encoders
 	double EncGetLeft();
 	double EncGetRight();
 	void EncResetLeft();
 	void EncResetRight();
 	void EncResetAll();
-
-	void Stop();
+	// Ultraonics
+	double GetUltraFrontLeft();
+	double GetUltraFrontRight();
 };
 
 #endif  // DriveTrain_H
